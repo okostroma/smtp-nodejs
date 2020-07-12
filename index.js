3cab3ad59d3953550896b293c66c8401417c40a4
Oxa <oxakost@gmail.com>
2020-07-12 22:18:10 +0300

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('https://smtp-nodejs-portfolio.herokuapp.com/', function (req, res) {
     res.send('Hello World!');
 });
 
